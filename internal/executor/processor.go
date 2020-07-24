2eae22c78db552591f8ce6970cbbdc329c3f8d53
SolarDomo <948872263@qq.com>
2020-07-24 10:33:53 +0800

    完成 Processor

---
+++ internal/executor/processor.go
@@ -0,0 +1,68 @@
+package executor
+
+import (
+	"github.com/sirupsen/logrus"
+	"runtime"
+	"sync"
+)
+
+type Processor struct {
+	cmdInChan  <-chan *Command
+	cmdOutChan chan<- *Command
+
+	once      sync.Once
+	wg        sync.WaitGroup
+	closeChan chan struct{}
+}
+
+func newProcessor(inChan <-chan *Command, outChan chan<- *Command) *Processor {
+	p := &Processor{
+		cmdInChan:  inChan,
+		cmdOutChan: outChan,
+		closeChan:  make(chan struct{}),
+	}
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go p.workRoutine(i)
+	}
+
+	return p
+}
+
+func (p *Processor) workRoutine(routineID int) {
+	logEntry := logrus.WithFields(logrus.Fields{
+		"ID": routineID,
+	})
+	logEntry.Debug("启动 Processor 协程")
+
+	var loop = true
+	for loop {
+		select {
+		case cmd, ok := <-p.cmdInChan:
+			if !ok {
+				continue
+			}
+
+			newCommandSlice := cmd.Process()
+			go p.outputCommands(newCommandSlice)
+
+		case <-p.closeChan:
+			loop = false
+		}
+	}
+
+	logEntry.Debug("关闭 Processor 协程")
+}
+
+func (p *Processor) outputCommands(commands []*Command) {
+	for _, command := range commands {
+		p.cmdOutChan <- command
+	}
+}
+
+func (p *Processor) WaitAndStop() {
+	p.once.Do(func() {
+		close(p.closeChan)
+	})
+	p.wg.Wait()
+}
